f1478c0c62616ddfffcbf1ab25b6c215a8648ba7
vvdanila87 <vvdanila87@gmail.com>
2017-02-06 21:51:19 +0200

    Internationalization and localization

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,23 +1,16 @@
-"""taskbuster URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
 from django.contrib import admin
-from .views import home
+from django.conf.urls.i18n import i18n_patterns
+from .views import home, home_files
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
+    url(r'i18n/', include('django.conf.urls.i18n')),
 ]
+
+urlpatterns += i18n_patterns(
+    url(r'^$', home, name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)
